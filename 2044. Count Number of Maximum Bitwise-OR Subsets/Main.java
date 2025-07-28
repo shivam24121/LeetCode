0667fbe3a04e5810e1689f9a041a8a55fe9c5a5c
shivam24121 <Shivam050905@gmail.com>
2025-07-28 19:58:52 +0530

    Create Main.java

---
+++ 2044. Count Number of Maximum Bitwise-OR Subsets/Main.java
@@ -0,0 +1,23 @@
+class Main {
+    public int countMaxOrSubsets(int[] nums) {
+        int n=nums.length;
+        int maxor=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            maxor|=nums[i];
+        }
+        int sub=1<<n;
+        for(int i=0;i<sub;i++){
+            int curor=0;
+            for(int j=0;j<n;j++){
+                if((i & (1<<j))!=0){
+                    curor|=nums[j];
+                }
+            }
+            if(curor==maxor){
+                count++;
+            }
+        }
+        return count;
+    }
+}
