f0765892ca9ed37e5065480014f964ba613a5a52
shivam24121 <Shivam050905@gmail.com>
2025-07-31 10:54:09 +0530

    Create Solution.java

---
+++ 53. Maximum Subarray/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res=nums[0];
+        int cur=nums[0];
+        for(int i=1;i<nums.length;i++){
+            cur=Math.max(nums[i],nums[i]+cur);
+            res=Math.max(res,cur);
+        }
+        return res;
+    }
+}
