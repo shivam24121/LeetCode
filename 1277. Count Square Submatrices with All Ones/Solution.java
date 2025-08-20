c229410edc085bcf90bc94c36f764b702d129c24
shivam24121 <Shivam050905@gmail.com>
2025-08-20 19:37:58 +0530

    Create Solution.java

---
+++ 1277. Count Square Submatrices with All Ones/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int res=0;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    dp[i][j]=0;
+                }
+                else if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                }
+                res+=dp[i][j];
+            }
+        }
+        return res;
+    }
+}
