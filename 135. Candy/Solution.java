a618201347dbd0439bcba1de57133f9704804a53
shivam24121 <Shivam050905@gmail.com>
2025-08-22 21:24:07 +0530

    Create Solution.java

---
+++ 135. Candy/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] res=new int[n];
+        int sum=0;
+        Arrays.fill(res,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                res[i]=res[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                res[i]=Math.max(res[i],res[i+1]+1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            sum+=res[i];
+        }
+        return sum;
+    }
+}
