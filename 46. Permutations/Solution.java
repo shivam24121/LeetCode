8edd9e4fa1c285b6a8d09be6a0a8fb48669e551e
shivam24121 <Shivam050905@gmail.com>
2025-10-01 21:18:07 +0530

    Create Solution.java

---
+++ 46. Permutations/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>>res=new ArrayList<>();
+        solve(nums,res,new ArrayList<>());
+        return res;
+    }
+    public void solve(int[] nums,List<List<Integer>>res,List<Integer>temp){
+        if(temp.size()==nums.length){
+            res.add(new ArrayList<>(temp));
+        }
+        else{
+            for(int i=0;i<nums.length;i++){
+                if(temp.contains(nums[i]))continue;
+                temp.add(nums[i]);
+                solve(nums,res,temp);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+}
