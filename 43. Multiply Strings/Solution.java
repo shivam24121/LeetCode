b22bc1744422ac3300e34881a7214bcf533cb4bf
shivam24121 <Shivam050905@gmail.com>
2025-10-26 19:06:22 +0530

    Create Solution.java

---
+++ 43. Multiply Strings/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        long a=Long.parseLong(num1);
+        long b=Long.parseLong(num2);
+        return String.valueOf(a*b);
+    }
+}
