a0c2aa0bfb5d6a0928b1d5cd82a3d452bb149439
shivam24121 <Shivam050905@gmail.com>
2025-11-17 20:02:50 +0530

    Create Solution.java

---
+++ 1437. Check If All 1's Are at Least Length K Places Away/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n=nums.length;
+        int prev=-(k+1);
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                if(i-prev-1<k){
+                    return false;
+                }
+                prev=i;
+            }
+        }
+        return true;
+    }
+}
