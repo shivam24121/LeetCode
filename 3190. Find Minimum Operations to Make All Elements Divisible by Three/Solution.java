567ef14f2f6d0540d8b57a4030cebaf2082c06f5
shivam24121 <Shivam050905@gmail.com>
2025-11-22 19:52:34 +0530

    Create Solution.java

---
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n=nums.length;
+        int cost=0;
+
+        for(int i=0;i<n;i++){
+            if((nums[i]%3)!=0){
+                cost++;
+            }
+        }
+        return cost;
+    }
+}
