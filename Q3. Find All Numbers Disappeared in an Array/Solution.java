9fce12de51c4c964cbb7b6d847ce38e17951f863
shivam24121 <Shivam050905@gmail.com>
2025-11-25 19:39:41 +0530

    Create Solution.java

---
+++ Q3. Find All Numbers Disappeared in an Array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer>res=new ArrayList<>();
+        int n=nums.length;
+        Set<Integer>set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        for(int i=1;i<=n;i++){
+            if(!set.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
