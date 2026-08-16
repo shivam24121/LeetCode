4e81a09da3017de6696e0307a0eb7f498ce62735
shivam24121 <Shivam050905@gmail.com>
2026-08-16 23:05:09 +0530

    Create Solution.java

---
+++ 2029. Stone Game IX/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0 = 0, c1 = 0, c2 = 0;
+
+        for (int x : stones) {
+            if (x % 3 == 0) c0++;
+            else if (x % 3 == 1) c1++;
+            else c2++;
+        }
+
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        return Math.abs(c1 - c2) > 2;
+    }
+}
